bf84eedab8d56f107ec27fd08f93cdf800b6a36f
keerthana2624 <keerthanamummy2002@gmail.com>
2024-08-28 15:02:09 +0530

    impplimented the code for triangle pattern using stars

--- pattern.py
+++ pattern.py
@@ -5,4 +5,14 @@ def squareprint(N):
         for j in range(N):
             print("*",end='')
         print()
-print(squareprint(5))
+# print(squareprint(5))
+
+
+# triangle pattern using stars(*)
+
+def triangle(N):
+    for i in range(N):
+        for j in range(i+1):
+            print("*",end='')
+        print()
+print(triangle(5))
