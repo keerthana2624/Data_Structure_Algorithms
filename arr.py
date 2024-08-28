dc44b75f5811cf4b747cdb3c3ac840c79992bda7
keerthana2624 <keerthanamummy2002@gmail.com>
2024-08-28 22:23:14 +0530

    implimented the code to removing the duplicates in the array

--- arr.py
+++ arr.py
@@ -11,7 +11,7 @@
 # # print(arr)
 
 
-
+# finding maximum element 
 def find_minmax(arr):
     max=arr[0]
     min=arr[0]
@@ -25,12 +25,22 @@ def find_minmax(arr):
 # max,min=find_minmax(arr)
 # print(f"min:{min},max:{max}")
 
-
+# arr is sorted or not
 def sortedornot(a):
     for i in range(len(a)-1):
         if a[i]>a[i+1]:
             return 0
     return 1
-print(sortedornot([1,10,3,4,2]))
+# print(sortedornot([1,10,3,4,2]))
+
+
+
+# removed duplicated from the arr
+def removeDuplivates(arr):
+    new_arr=sorted(set(arr))
+    for i in range(len(new_arr)):
+        arr[i]=new_arr[i]
+    return new_arr
+print(removeDuplivates([1,3,2,4,3,2]))
 
 
