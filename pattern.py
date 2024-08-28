e75e30a8e35ec6674e6f90265337aea562233420
keerthana2624 <keerthanamummy2002@gmail.com>
2024-08-28 15:20:43 +0530

    printed triangle which takes an integer n  as the input

--- pattern.py
+++ pattern.py
@@ -16,3 +16,15 @@ def triangle(N):
             print("*",end='')
         print()
 print(triangle(5))
+
+
+# printTriangle() which takes an integer n  as the input
+
+def trianglewithnum(N):
+    for i in range(N):
+        num=1
+        for j in range(i+1):
+            print(num,end='')
+            num+=1
+        print()
+print(trianglewithnum(5))
