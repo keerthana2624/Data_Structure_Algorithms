70f52f90cbbce2cb82f3623aaefe90366c7180d1
keerthana2624 <keerthanamummy2002@gmail.com>
2024-12-23 17:10:25 +0530

    given array is sorted in the non-decreasing order or no logic is implimented

--- Array.py
+++ Array.py
@@ -10,11 +10,21 @@ def largestNum(arr):
 # print(largestNum([2,5,3,7]))
 
 #  2.Second Largest Number
-
 def sec_smallLarge(arr):
     sort=sorted(arr)
     sec_small=sort[1]
     sec_large=sort[-2]
     return(sec_small,sec_large)
-x=[4,2,6,8,99]
-print(sec_smallLarge(x))
+# x=[4,2,6,8,99]
+# print(sec_smallLarge(x))
+
+#  3.Check Sorted Array
+
+def check_sorted(arr):
+    for i in range(len(arr)):
+        if arr[i]>arr[i+1]:
+            return False
+        else:
+            return True
+print(check_sorted([4,7,9]))
+print(check_sorted([45,3,9]))
