a5bc51390ae427e824dc4ee395ec63211fac8df2
keerthana2624 <keerthanamummy2002@gmail.com>
2025-01-07 11:39:50 +0530

    implimented code for finding the sum of elements in array

--- Array.py
+++ Array.py
@@ -1,33 +1,4 @@
-# 1.Largest Element in the Array
 
-def largestNum(arr):
-    largestNum=arr[0]
-    for i in arr:
-        if i > largestNum:
-            largestNum=i
-    else:
-        return largestNum
-# print(largestNum([2,5,3,7]))
-
-#  2.Second Largest Number
-def sec_smallLarge(arr):
-    sort=sorted(arr)
-    sec_small=sort[1]
-    sec_large=sort[-2]
-    return(sec_small,sec_large)
-# x=[4,2,6,8,99]
-# print(sec_smallLarge(x))
-
-#  3.Check Sorted Array
-
-def check_sorted(arr):
-    for i in range(len(arr)):
-        if arr[i]>arr[i+1]:
-            return False
-        else:
-            return True
-# print(check_sorted([4,7,9]))
-# print(check_sorted([45,3,9]))
 
 #  Remove Duplicates from Sorted Array
 def removeDuplicates(arr):
@@ -78,3 +49,23 @@ def sec_large(array):
     array.sort()
     return array[-2]
 # print(sec_large([2,4,1,7,5]))
+
+# 4. Check if an array is sorted. 
+def check_sorted(arr):
+    for i in range(len(arr)-1):
+        if arr[i]>arr[i+1]:
+            return False
+    return True
+# print(check_sorted([1,2,3,4,5,6]))
+# print(check_sorted([1,6,3,4,9,6]))
+
+
+# 5. Find the sum of all elements in an array.  
+
+def SumOfElements(arr):
+    sum=0
+    for i in arr:
+        sum+=i
+    return sum
+print(SumOfElements([2,4,2,5,1,1]))
+        
